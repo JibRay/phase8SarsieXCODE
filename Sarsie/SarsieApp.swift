6c4384df6046a22cc04d592c4ecab71a5aacaae3
Jib Ray <jib2ray@gmail.com>
2023-03-23 15:03:37 -0700

    Initial Commit

---
+++ Sarsie/SarsieApp.swift
@@ -0,0 +1,17 @@
+//
+//  SarsieApp.swift
+//  Sarsie
+//
+//  Created by Jib Ray on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct SarsieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
