8fff0f761d861817dd5b195f8f8f29e14aaf1d8b
Jib Ray <jib2ray@gmail.com>
2023-04-11 13:30:47 -0700

    Removed red LED. Changed button to round, yellow. Added log output text field and camera image. Added files from Apple sample app.

--- Sarsie/ContentView.swift
+++ Sarsie/ContentView.swift
@@ -8,26 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var outputLog: String =
+    " 1 Sarsie version 1\n"
+    + " 2 Output log\n"
+    + " 3 A place to\n"
+    + " 4 write debug info.\n"
+    + " 5 This will be removed\n"
+    + " 6 in the published\n"
+    + " 7 version. This can\n"
+    + " 8 display intermediate\n"
+    + " 9 results and progress\n"
+    + "10 information.\n"
+    
     var body: some View {
         ZStack {
-            // Color.black.edgesIgnoringSafeArea(.all)
             Color.black.ignoresSafeArea(.all)
             VStack {
-                Spacer()
-                    .frame(maxHeight: 50)
-                Rectangle()
-                    .fill(Color(red: 1, green: 0, blue: 0))
-                    .frame(width: 50, height: 50)
-                Spacer()
-                Button("Start", action: {})
+                HStack {
+                    Spacer()
+                    ZStack {
+                        // This button initiates the test.
+                        Button ("", action: {})
+                            .frame (width: 150, height: 150)
+                            .background(.yellow)
+                            .clipShape(Circle())
+                        Circle()
+                            .strokeBorder(.white, lineWidth: 3)
+                            .frame(width: 165, height: 165)
+                        
+                    }
                     .padding()
-                    .font(.system(size: 60))
-                    .background(Color.white)
-                    .foregroundColor(Color.black)
-                    .clipShape(Capsule())
+                }
+                    Text("SARSIE")
+                        .bold()
+                        .font(.system(size: 40))
+                        .foregroundColor(.white)
+                    // Comment this TextField and Image for release version.
+                    TextField("log", text: $outputLog, axis: .vertical)
+                        //.foregroundColor(.blue)
+                        .background(.gray)
+                        .frame(width: 300, height: 150)
+                Image("ArmoredCarp01")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300, height: 200)
+                    // End comment section.
+                    Spacer()
             }
-            .padding()
         }
+        .buttonStyle(.plain)
     }
 }
 
